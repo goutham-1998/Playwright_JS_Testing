2aebcb75c6a2345ecf002408984d81e9e5f31499
Goutham Gummadipudi <ggummadipudi@gmail.com>
2026-02-04 23:47:48 +0530

    04-02-2026

--- tests/first_Test.spec.js
+++ tests/first_Test.spec.js
@@ -19,7 +19,7 @@ test("Browser Fixture Test Two", async ({ page }) => {
     await userName.fill('rahulshettyacademy');
     await pwd.fill('Learning@830$3mK2000');
     await signIn.click({ timeout: 1000 });
-    console.log(await page.locator("[style*='block']").textContent()); //textContent() - Used to extract test from the message
+    console.log(await page.locator("[style*='block']").textContent()); //textContent() - Used to extract text from the message
     await expect(page.locator("[style*='block']")).toContainText("Incorrect") //An assertion to compare the text with the sub-string
 
 
@@ -36,26 +36,73 @@ test("Browser Fixture Test Two", async ({ page }) => {
     const allTitles = await page.locator(".card-body a").allTextContents();
     console.log(allTitles);
     await expect(page.locator(".card-body a").nth(0)).toContainText("iphone X");
-    
-    
+
+
 
 
 });
 
-test("Login Practice - 1",async ({page}) => {
+test("Login Practice - 1", async ({ page }) => {
 
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
     await page.locator("#userEmail").fill("ggummadipudi@gmail.com");
     await page.locator("#userPassword").fill("Password123");
     await page.locator("#login").click();
     //console.log(await page.locator("[style*='text-transform']").first().textContent()); 
-   
+
     //While using allTextContents() method the return type is Array. Array[0] is also valid. While the page is loading, this method won't auto-wait and returns the blank page as an Array without any elements
-     
-    // await page.waitForLoadState("networkidle"); // To overome the allTextContents() issue, we can use waitForLoadState("networkidle") which waits until the network calls halt
+
+    // await page.waitForLoadState("networkidle"); // To overome the allTextContents() method issue, we can use waitForLoadState("networkidle") which waits until the network calls halt
     await page.locator("[style*='text-transform']").last().waitFor(); // As waitForLoadState("networkidle") sometimes might lead to flaky, we can use waitfor() method and we can hint that until which element it has to wait before auto-timing out.
     const allTitles = await page.locator("[style*='text-transform']").allTextContents();
     console.log(allTitles)
 
-        
-    });
+
+});
+
+
+test("UI Controls", async ({ page }) => {
+
+    const userName = page.locator('#username');
+    const signIn = page.locator('#signInBtn');
+    const pwd = page.locator("[name='password']");
+    const blinkText = page.locator("[href*= 'documents-request']");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await userName.fill('rahulshettyacademy');
+    await pwd.fill('Learning@830$3mK2000');
+    await page.locator(".radiotextsty").last().click();
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+    //console.log(expect(page.locator(".radiotextsty").last().isChecked()));
+    await page.locator("#okayBtn").click();
+    const dropDown = await page.locator("select.form-control")
+    await dropDown.selectOption("Consultant");
+    await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+    //await page.locator("#terms").uncheck();
+    await expect(blinkText).toHaveAttribute("class", "blinkingText");
+
+
+
+
+});
+
+test.only("Handling Child Windows", async ({ browser }) => {
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const blinkText = await page.locator("[href*= 'documents-request']");
+
+    const [newPage] = await Promise.all([context.waitForEvent("page"), blinkText.click(),]);
+
+    const redText = await newPage.locator("[class='im-para red']").textContent();
+    console.log(redText);
+
+
+
+
+
+
+
+
+});
